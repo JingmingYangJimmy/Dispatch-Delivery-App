c99e2039fdedcc6ebbaa01ef223540a27d2e29cb
gregjones11235 <gregjones11235@gmail.com>
2025-09-28 19:11:13 -0400

    1

--- src/main/java/com/delivery/routeplanning/repository/StationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.delivery.routeplanning.repository;
-
-import com.delivery.routeplanning.entity.Station;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StationRepository extends JpaRepository<Station, String> {
-}
