97cabe7d501f87d5971d87a242882def05bb93c2
gregjones11235 <gregjones11235@gmail.com>
2025-09-28 21:54:27 -0400

    1

--- routeplanning_complete/routeplanning_complete/readme.md
+++
@@ -1,3 +0,0 @@
-注意，在application.properties中：
-1. google.maps.api.key要去Google Maps Platform官网上获取一个，然后填入自己的API KEY
-2. spring.datasource.url、spring.datasource.username、spring.datasource.password要改成自己的
--- routeplanning_complete/routeplanning_complete/src/main/java/com/delivery/repository/StationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.delivery.repository;
-
-import com.delivery.entity.Station;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StationRepository extends JpaRepository<Station, String> {
-}
